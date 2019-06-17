2a9c0ff1c93c970ad747904d343f385ed985d424
John Johnson <codeelegance@gmail.com>
2019-06-17 16:41:00 -0400

    Initial push of bootstrap 4 theme

--- themes/custom/duson/gulpfile.js
+++ themes/custom/duson/gulpfile.js
@@ -57,7 +57,7 @@ function js () {
 // Static Server + watching scss/html files
 function serve () {
   browserSync.init({
-    proxy: 'http://local.drupal.edu',
+    proxy: 'https://ser.nursing.duke.edu',
   })
 
   gulp.watch([paths.scss.watch, paths.scss.bootstrap], styles).on('change', browserSync.reload)
